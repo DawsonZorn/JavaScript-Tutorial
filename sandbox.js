e81b6c4d5adb215f404bd43c07cfd7f84ccc106b
Dawson Zorn <dawsonzorn@outlook.com>
2025-01-07 10:55:31 -0600

    added a if statement and an array and usage of console.log

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+let userName = prompt("Enter your name: ");
+if (userName != '') {
+  alert("Hello " + userName);
+}
+
+let goat = Messi;
+
+let message = `Hello, ${goat}`;
+
+let favoritePlayers = ['Messi', 'Mitoma', 'Robben'];
+console.log(favoritePlayers[0]); // Messi
+
